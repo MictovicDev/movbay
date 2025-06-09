2cb996dbe9a0687b404daac364f67a87777dbc75
Michael <awaemekamichael@gmail.com>
2025-06-09 11:23:45 +0100

    changes

--- users/utils/redis_cli.py
+++ users/utils/redis_cli.py
@@ -1,8 +1,20 @@
+from pathlib import Path
 import redis
 import os
+from dotenv import load_dotenv
+from datetime import timedelta
+
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+
+load_dotenv(BASE_DIR / ".env")
+
+
 
 redis_client = redis.StrictRedis(
-    host=os.getenv("REDIS_HOST", "localhost"),
+    host=os.getenv("REDIS_HOST", "redis"),
     port=int(os.getenv("REDIS_PORT", 6379)),
     db=0,
     decode_responses=True
